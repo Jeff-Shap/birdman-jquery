7173316632e31368e6f80a9e97ecc1151d4caf64
Jeff Shapiro <MacArthur1@MacArthur-Jeffs-MacBook-Pro.local>
2015-04-29 16:13:01 -0400

    added drope menu

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-var main = function() {
-  
-}
+var main = function clickMenu() {
+    $('img').click(function(){
+        console.log("click");
+        $('.dropdown-menu').toggle();
+    })
+};
+
  
 $(document).ready(main);
